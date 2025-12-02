ec0f18749c66bd402ae27cdc891ceba5ab669630
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2025-12-02 13:16:25 +0300

    added icon and botton

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,20 +1,37 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.TreeMap
 
 class MainActivity : AppCompatActivity() {
+
+    fun Int.toShortNum(): String {
+        if(this < 1000){
+            return this.toString()
+        }
+        val suffix = TreeMap<Long, String>()
+        suffix[1000L] = "K"
+        suffix[1000000L] = "M"
+        val entry = suffix.floorEntry(this.toLong()) ?: return this.toString()
+
+        val base = entry.key
+        val suffixes = entry.value
+        val formatted = this.toDouble() / base
+        return String.format("%.1f%s", formatted, suffixes).replace(".0", "")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val countLikes: TextView = findViewById(R.id.countlikes)
+        formatLikesFromResources(countLikes)
+    }
+
+    private fun formatLikesFromResources(textView: TextView) {
+        val rawLikes = resources.getInteger(R.integer.post_likes)
+        val formattedLikes = rawLikes.toShortNum()
+        textView.text = formattedLikes
     }
 }
