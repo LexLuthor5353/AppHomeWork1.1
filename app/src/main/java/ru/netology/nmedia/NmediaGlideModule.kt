2dd782c9679d5a11ced32999b6cd5d90d37d9034
Aleksandr Karkhunen <ajlvatanabe@gmail.com>
2026-04-12 10:42:56 +0300

    Glide,Rounded-and-Attachments

---
+++ app/src/main/java/ru/netology/nmedia/NmediaGlideModule.kt
@@ -0,0 +1,28 @@
+package ru.netology.nmedia
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import okhttp3.OkHttpClient
+import java.io.InputStream
+import java.util.concurrent.TimeUnit
+
+@GlideModule
+class NmediaGlideModule : AppGlideModule() {
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .callTimeout(120, TimeUnit.SECONDS)
+            .build()
+        registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(client),
+        )
+    }
+}
